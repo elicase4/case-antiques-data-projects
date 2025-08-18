4fe09ac8f603c9d0d29a6220c02173b3a059a269
Eli Case <elicase4@gmail.com>
2025-08-18 00:36:45 -0500

    edit: data loader, add: bisquare invoice eda

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,53 @@
-import pandas as pd
-from gspread_pandas import Spread, conf
+import gspread
+import pandas
 
-def load_google_sheet(sheet_name, worksheet_name=None, credentials_path="google_credentials.json"):
-    spread = Spread(sheet_name, config=conf.get_config(conf_dir='.', file_name=credentials_path))
-    df = spread.sheet_to_df(index=None, sheet=worksheet_name)
+from pathlib import Path
+from google.oauth2.service_account import Credentials
+
+
+def load_google_sheet(sheet_id: str, worksheet_name: str = None) -> pandas.DataFrame:
+    """
+    Loads data from a specified Google Sheet into a pandas DataFrame.
+
+    This function uses a service account to authenticate with the Google Sheets API
+    and retrieve data from a specified sheet. The sheet must be shared with the
+    service account email in advance. All records in the selected worksheet are returned
+    as rows in a DataFrame.
+
+    Args:
+        sheet_id (str): The id of the Google Spreadsheet (identifiable by the Google Sheet url).
+        worksheet_name (str, optional): The name of the worksheet/tab within the sheet.
+            If None, the first worksheet (`sheet1`) will be used.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the data from the worksheet, with headers
+        inferred from the first row.
+
+    Raises:
+        gspread.SpreadsheetNotFound: If the specified spreadsheet does not exist or
+            is not shared with the service account.
+        gspread.WorksheetNotFound: If the specified worksheet name does not exist in
+            the spreadsheet.
+        google.auth.exceptions.GoogleAuthError: If authentication with the service
+            account fails.
+    """
+
+    credentials_path = Path(__file__).resolve(
+    ).parents[1] / "auth" / "case-antiques-ml-projects-sheets-reader-credentials.json"
+
+    SCOPES = [
+        "https://www.googleapis.com/auth/spreadsheets.readonly",
+        "https://www.googleapis.com/auth/drive.readonly"
+    ]
+
+    creds = Credentials.from_service_account_file(
+        credentials_path, scopes=SCOPES)
+
+    google_creds = gspread.authorize(creds)
+    sheet = google_creds.open_by_key(sheet_id)
+    worksheet = sheet.sheet1 if worksheet_name is None else sheet.worksheet(
+        worksheet_name)
+    data = worksheet.get_all_records()
+    df = pandas.DataFrame(data)
+
     return df
